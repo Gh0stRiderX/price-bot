9de2e24e28b608319891e394302cb4e2f6bdae70
Gh0stRiderX <warthog.wow@gmaiL.com>
2020-04-13 17:00:27 +0200

    feat: adding Cdiscount Support

---
+++ cdiscount.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/chromedp/chromedp"
+	"strconv"
+	"time"
+)
+
+type Cdiscount struct {
+	productUrl string
+	minPrice   float64
+}
+
+func (cd *Cdiscount) Name() string {
+	return "CDISCOUNT"
+}
+
+func (cd *Cdiscount) MinPrice() float64 {
+	return cd.minPrice
+}
+
+func (cd *Cdiscount) FetchPrice(ctx context.Context) (float64, error) {
+	p, err := cd.getPrice(ctx)
+	if err != nil {
+		return InvalidPrice, fmt.Errorf("could not fetch price, got error %v", err)
+	}
+	return p, nil
+}
+
+func (cd *Cdiscount) getPrice(ctx context.Context) (float64, error){
+	var price string
+	err := chromedp.Run(ctx,
+		chromedp.Navigate(cd.productUrl),
+		chromedp.Sleep(2*time.Second),
+		chromedp.Evaluate(cd.getPriceJS(), &price))
+	if err != nil {
+		return InvalidPrice, err
+	}
+	return strconv.ParseFloat(price, 64)
+}
+
+func (cd *Cdiscount) getPriceJS() string{
+	return `
+		document.getElementsByClassName("price")[0].innerText.replace('€', '.')
+	`
+}

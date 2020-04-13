444f21837ef76ab1d7544505d31274e17a1a75ec
Gh0stRiderX <warthog.wow@gmaiL.com>
2020-04-13 17:53:21 +0200

    feat: enabling Rueducommerce in loop

--- rueducommerce.go
+++ rueducommerce.go
@@ -4,28 +4,23 @@ import (
 	"context"
 	"fmt"
 	"github.com/chromedp/chromedp"
-	"strconv"
 	"time"
 )
 
-type RueDuCommerce struct {
+type Rueducommerce struct {
 	productUrl string
 	minPrice   float64
 }
 
-func (ru *RueDuCommerce) Name() string {
-	return "RUE_DU_COMMERCE"
+func (ru *Rueducommerce) Name() string {
+	return "RUEDUCOMMERCE"
 }
 
-func (ru *RueDuCommerce) MinPrice() float64 {
+func (ru *Rueducommerce) MinPrice() float64 {
 	return ru.minPrice
 }
 
-func (ru *RueDuCommerce) IsAvailable(_ context.Context) (bool, error) {
-	return true, nil // TODO
-}
-
-func (ru *RueDuCommerce) FetchPrice(ctx context.Context) (float64, error) {
+func (ru *Rueducommerce) FetchPrice(ctx context.Context) (float64, error) {
 	p, err := ru.getPrice(ctx)
 	if err != nil {
 		return InvalidPrice, fmt.Errorf("could not fetch price, got error %v", err)
@@ -33,18 +28,26 @@ func (ru *RueDuCommerce) FetchPrice(ctx context.Context) (float64, error) {
 	return p, nil
 }
 
-func (ru *RueDuCommerce) getPrice(ctx context.Context) (float64, error) {
-	var price string
-	var ok bool
+func (ru *Rueducommerce) getPrice(ctx context.Context) (float64, error) {
+	var price float64
 	err := chromedp.Run(ctx,
 		chromedp.Navigate(ru.productUrl),
 		chromedp.Sleep(2*time.Second),
-		chromedp.AttributeValue("[itemprop='price']", "content", &price, &ok))
-	if err != nil {
-		return InvalidPrice, err
-	}
-	if !ok {
-		return InvalidPrice, fmt.Errorf("failed to retrieve attribute value")
-	}
-	return strconv.ParseFloat(price, 64)
+		chromedp.Evaluate(ru.getPriceJS(), &price))
+	return price, err
+}
+
+func (ru *Rueducommerce) getPriceJS() string {
+	return fmt.Sprintf(`
+function convertPriceStringToFloat(price) {
+    return parseFloat(price.replace('€', '.'))
+}
+
+function getPrice() {
+    const priceElement = document.getElementsByClassName("price-pricesup")[0]
+    return priceElement && priceElement.innerText ? convertPriceStringToFloat(priceElement.innerText) : %d;
+}
+
+getPrice();
+`, InvalidPrice)
 }
